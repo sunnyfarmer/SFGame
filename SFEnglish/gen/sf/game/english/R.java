625a66e9d71bb87541211e565f87476703c60f01
sunnyfarmer <84193866@qq.com>
2013-09-30 16:57:20 +0800

    界面

--- SFEnglish/gen/sf/game/english/R.java
+++ SFEnglish/gen/sf/game/english/R.java
@@ -8,6 +8,10 @@
 package sf.game.english;
 
 public final class R {
+    public static final class anim {
+        public static final int btn_un_clickable=0x7f040000;
+        public static final int iv_go=0x7f040001;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -17,14 +21,18 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int content_size=0x7f050002;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090003;
+        public static final int btnNext=0x7f090002;
+        public static final int btnPrevious=0x7f090000;
+        public static final int ivCourse=0x7f090001;
     }
     public static final class layout {
         public static final int activity_course=0x7f030000;
@@ -32,13 +40,17 @@ public final class R {
         public static final int activity_practice=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int confirm=0x7f050003;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int charge_tips=0x7f060006;
+        public static final int confirm=0x7f060003;
+        public static final int default_content_description=0x7f060007;
+        public static final int hello_world=0x7f060002;
+        public static final int next=0x7f060005;
+        public static final int previous=0x7f060004;
     }
     public static final class style {
         /** 
@@ -50,22 +62,11 @@ public final class R {
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
         
-
-        Base application theme for API 11+. This theme completely replaces
-        AppBaseTheme from res/values/styles.xml on API 11+ devices.
-    
- API 11 theme customizations can go here. 
-
-        Base application theme for API 14+. This theme completely replaces
-        AppBaseTheme from BOTH res/values/styles.xml and
-        res/values-v11/styles.xml on API 14+ devices.
-    
- API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- SFEnglish/src/sf/game/english/ActivityHome.java
+++ SFEnglish/src/sf/game/english/ActivityHome.java
@@ -1,13 +1,63 @@
 package sf.game.english;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ActivityHome extends TopActivity {
+	public static final String TAG = "ActivityHome";
+
+	protected Button btnPrevious = null;
+	protected Button btnNext = null;
+	protected ImageView ivCourse = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+		super.onCreate(savedInstanceState);
 	}
 
+	@Override
+	protected void initData() {
+		// TODO Auto-generated method stub
+		super.initData();
+	}
+	@Override
+	protected void initView() {
+		super.initView();
+		this.btnPrevious = (Button) this.findViewById(R.id.btnPrevious);
+		this.ivCourse = (ImageView) this.findViewById(R.id.ivCourse);
+		this.btnNext = (Button) this.findViewById(R.id.btnNext);
+	}
+	@Override
+	protected void setListener() {
+		super.setListener();
+		this.btnPrevious.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Animation animation = AnimationUtils.loadAnimation(ActivityHome.this, R.anim.btn_un_clickable);
+				v.startAnimation(animation);
+			}
+		});
+		this.btnNext.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Animation animation = AnimationUtils.loadAnimation(ActivityHome.this, R.anim.btn_un_clickable);
+				v.startAnimation(animation);
+			}
+		});
+		this.ivCourse.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Animation animation = AnimationUtils.loadAnimation(ActivityHome.this, R.anim.iv_go);
+				v.startAnimation(animation);
+			}
+		});
+	}
 }
